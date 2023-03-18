bed8c8b770468b84e6ceac7ae3cf173d37cd003c
mdabdulhalim <htmlcssjs2023>
2023-03-18 16:35:36 +0600

    set logic in blade

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -6,5 +6,35 @@
 @endsection
 
 @Section('content')
-<h2>User blade</h2>
+{{-- <h1>This is user </h1> --}}
+
+
+{{-- @if(1 == 1)
+    Yes this is done!
+@endif --}}
+
+
+{{-- @if(1==2)
+    This is if part
+@elseif( 1==1)
+    this is else part
+
+@endif --}}
+
+
+@if(1==2)
+    This is if part
+@else
+    this is else part
+
+@endif
+
+@endsection
+
+
+
+@section('scripts')
+    <script>
+        @json({{$b}}); // convert into json 
+    </script>
 @endsection
